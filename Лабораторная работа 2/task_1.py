8b3ece8de72ad7791a1910e90a34462460fe2d95
Ivan Simonenko <45663683+simonoffcc@users.noreply.github.com>
2023-10-11 21:12:59 +0300

    Лабораторная работа 2 (Условия и циклы)
    В обоих файлах есть 14-ые строки, которые являются комментариями. По моему мнению, эти варианты реализации предшествующих строк записи результата разницы diff в money_capital являются более читабельными и подходящими под условие задачи. Однако я не стал их использовать, потому что был не уверен в возможности использования функции abs() для нахождения модуля числа.

---
+++ Лабораторная работа 2/task_1.py
@@ -0,0 +1,21 @@
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
+
+# TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
+
+count_of_month = 0
+
+while True:
+    diff = salary - spend
+    money_capital += diff
+
+    if money_capital <= 0:
+        break
+    else:
+        count_of_month += 1
+
+    spend += spend * increase
+
+print("Количество месяцев, которое можно протянуть без долгов:", count_of_month)
---
+++ Лабораторная работа 2/task_2.py
@@ -0,0 +1,18 @@
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+months = 10  # Количество месяцев, которое планируется протянуть без долгов
+increase = 0.03  # Ежемесячный рост цен
+
+# TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
+
+money_capital = 0
+
+for _ in range(months):
+    diff = salary - spend
+    money_capital -= diff
+    
+    # money_capital += diff
+
+    spend += spend * increase
+
+print(f"Подушка безопасности, чтобы протянуть {months} месяцев без долгов:", round(money_capital))
